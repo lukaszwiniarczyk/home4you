8977685bb8bf51496382baba3cb0d6b41478bfa5
Łukasz Winiarczyk <lukaszwiniarczyk05@gmail.com>
2025-06-06 21:37:26 +0200

    added header and nav for mobile

---
+++ src/js/script.js
@@ -0,0 +1,25 @@
+const navBtn = document.querySelector('.nav__btn');
+const navMobile = document.querySelector('.nav__links');
+const navMobileLinks = document.querySelectorAll('.nav__links-link');
+const body = document.querySelector('body');
+const html = document.querySelector('html');
+
+
+navBtn.addEventListener('click', () => {
+	navMobile.classList.toggle('nav__links-active');
+
+	if (navMobile.classList.contains('nav__links-active')) {
+		body.style.overflow = 'hidden';
+		html.style.overflow = 'hidden';
+	} else {
+		body.style.overflow = 'visible';
+		html.style.overflow = 'visible';
+	}
+});
+navMobileLinks.forEach((item) => {
+	item.addEventListener('click', () => {
+		navMobile.classList.remove('nav__links-active');
+		body.style.overflow = 'visible';
+		html.style.overflow = 'visible';
+	});
+});
